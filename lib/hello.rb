6d6ecc3297388c6a551871a859fae632f6432372
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-03 19:16:06 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,13 @@
-def hello_t
-
+def hello_t(names)
+  if block_given?
+    arr = []
+    names.each do |name|
+      yield(name)
+    end
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
-
+hello_t(["Tim", "Tom", "Jim"]) { |n| puts n }
+hello_t(["Ali", "Jasmine", "Persephone"]) { |n| puts n.upcase }
